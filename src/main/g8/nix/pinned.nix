f2dd3db89240e27d2a517bb5d9ffcc11776eb65c
Sadiq Ali Shaik <sadiq.computers@gmail.com>
2021-09-12 18:22:06 +0530

    Bump sbt and other versions

--- src/main/g8/nix/pinned.nix
+++ src/main/g8/nix/pinned.nix
@@ -1,13 +1,13 @@
 {
   nixpkgs = fetchTarball {
-    name   = "nixos-unstable-2020-09-25";
-    url    = "https://github.com/NixOS/nixpkgs-channels/archive/72b9660dc18b.tar.gz";
-    sha256 = "1cqgpw263bz261bgz34j6hiawi4hi6smwp6981yz375fx0g6kmss";
+    name   = "nixos-unstable-2021-09-11";
+    url    = "https://github.com/NixOS/nixpkgs/archive/f49dfac.tar.gz";
+    sha256 = "02jw1vban780kdirz7xfa2g9x6m3gqfn53nzapp7flgzp0i2vvpa";
   };
 
   sbt-derivation = fetchTarball {
-    name   = "sbt-derivation-2020-10-08";
-    url    = "https://github.com/zaninime/sbt-derivation/archive/9666b2b.tar.gz";
-    sha256 = "17r74avh4i3llxbskfjhvbys3avqb2f26pzydcdkd8a9k204rg9z";
+    name   = "sbt-derivation-2021-04-03";
+    url    = "https://github.com/zaninime/sbt-derivation/archive/920b6f1.tar.gz";
+    sha256 = "0apg8mk7bzq418c1jyq5s5c3sp6bh4smirwxi29dhbnyp8q9ddv7";
   };
 }
